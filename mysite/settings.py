65994fbb9711d6bc721f63bc8fe6207b93fc51db
misha <meja1@inbox.ru>
2022-04-24 15:49:27 +0300

    secret_key)

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-
+from secret_key import _key
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&^x66lx)i4_9p!o60!*wyn%8al@gb=v22!g&zl%@q$_0jw2is5'
+SECRET_KEY = _key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ secret_key.py
@@ -0,0 +1,1 @@
+_key = 'django-insecure-&^x66lx)i4_9p!o60!*wyn%8al@gb=v22!g&zl%@q$_0jw2is5'
